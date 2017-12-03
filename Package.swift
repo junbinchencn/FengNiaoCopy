c2b6ccf14025d3129802a54f7af34381351ff769
jim <junbinchencn@gmail .com>
2017-12-03 16:50:11 +0800

    添加依赖
    FengNiaoCopy 依赖 Rainbow,CommandLine,Spectre,PathKit 这四个第三方库

--- Package.swift
+++ Package.swift
@@ -1,8 +1,18 @@
-
 // swift-tools-version:3.1
 
 import PackageDescription
-
 let package = Package(
-    name: "FengNiaoCopy"
+    name: "FengNiaoCopy",
+    targets: [
+        Target(name: "FengNiaoKitCopy", dependencies: []),
+        Target(name: "FengNiaoCopy", dependencies: ["FengNiaoKitCopy"])
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2),
+        .Package(url: "https://github.com/jatoben/CommandLine", "3.0.0-pre1"),
+        .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8)
+    ],
+    exclude: [ "Tests/Fixtures" ]
 )
+
